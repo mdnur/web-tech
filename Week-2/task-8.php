10fe61cce8dddfd741580865d43db246465c0422
Mohammad Nur <mdnur701@gmail.com>
2023-06-13 11:33:31 +0600

    task-8 done

--- Week-2/task-8.php
+++ Week-2/task-8.php
@@ -2,11 +2,31 @@
 // Not Done
 
 $array = array(
-    array(1,2,3,"A"),
-    array(1,2,"B","C"),
-    array(1,"D","E","F"),
+    array(1, 2, 3, "A"),
+    array(1, 2, "B", "C"),
+    array(1, "D", "E", "F"),
 );
 
+
+
+// foreach ($array as $row) {
+//     foreach ($row as $element) {
+//         if (is_string($element)) {
+//             echo $element . " ";
+//         }
+//     }
+//     echo "\n";
+// }
+
+for ($i = 0; $i < 3; $i++) {
+
+    for ($j = 0; $j < 4; $j++) {
+        if (is_string($array[$i][$j])) {
+            echo $array[$i][$j] . " ";
+        }
+    }
+    echo "\n";
+}
 
 /**
  1 2 3
@@ -14,14 +34,9 @@ $array = array(
  1 
  */
 
-$rows = 5;
-echo $array[0][0] ." ". $array[0][1] . " " . $array[0][2] . "\n";
-echo $array[1][0] ." ". $array[1][1] . " \n" ;
-echo $array[2][0] . "\n";
-
 for ($i = 0; $i < 3; $i++) {
 
-    for ($j = 0; $j <= $i-$j; $j++) {
+    for ($j = 0; $j < 3-$i; $j++) {
         echo ($array[$i][$j] . " ");
     }
     echo "\n";
--- Week-2/tempCodeRunnerFile.php
+++ Week-2/tempCodeRunnerFile.php
@@ -1,17 +1,9 @@
 <?php
-
-/** 7. Print the following shapes Hints: use NESTED LOOP.  
- * 
- *
- */
-
-$rows = 5;
-
-for ($i = $rows - 1; $i >= 0; $i--) {
-    $sample = 1;
-    for ($j = 0; $j <= $i; $j++) {
-        //prints star and space
-        echo ($sample++ . " ");
-    }
-    echo "\n";
-}
+foreach ($array as $row) {
+//     foreach ($row as $element) {
+//         if (is_string($element)) {
+//             echo $element . " ";
+//         }
+//     }
+//     echo "\n";
+// }
